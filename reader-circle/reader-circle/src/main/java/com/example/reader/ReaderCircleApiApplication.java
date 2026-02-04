47c1e5250684f6b154cba287cdc3cd5d58be57ad
Sadid Rafan <sadidrafan094@gmail.com>
2026-02-04 20:29:19 +0600

    project configure

---
+++ reader-circle/reader-circle/src/main/java/com/example/reader/ReaderCircleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderCircleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderCircleApiApplication.class, args);
+	}
+
+}
---
+++ reader-circle/reader-circle/src/test/java/com/example/reader/ReaderCircleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderCircleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
